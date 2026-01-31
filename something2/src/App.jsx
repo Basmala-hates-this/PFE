4e6bfbc50ccab8ee05abd43833d70dce2ae219bc
Basmala-hates-this <laouaribasmala75@gmail.com>
2026-01-31 20:03:00 +0100

    now...to the bigger fight...wishe me luck

--- something2/src/App.jsx
+++ something2/src/App.jsx
@@ -1,28 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
- import Welcome from './pages/Welcome.jsx';
-import Info from './pages/Info.jsx';
-import Register from './pages/Register.jsx';
-import Login from './pages/Login.jsx';
-import Fin from './pages/Fin.jsx';
-import RRP from './pages/RRP.jsx';
-import Reset from './pages/Reset.jsx';
-import Dashboard from './pages/Dashboard.jsx';
-import Profile from './pages/Profile.jsx';
-
+import Welcome from "./pages/Welcome.jsx";
+import Info from "./pages/Info.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import Fin from "./pages/Fin.jsx";
+import RRP from "./pages/RRP.jsx";
+import Reset from "./pages/Reset.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Profile from "./pages/Profile.jsx";
 
 function App() {
-  
   return (
-    // <Welcome />
-    // <Info />
-      // <Register />
-    // <Login />
-    // <Fin />
-    // <RRP />
-    // <Reset />
-    <Dashboard />
-    // <Profile />
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/fin" element={<Fin />} />
+        <Route path="/rrp" element={<RRP />} />
+        <Route path="/reset" element={<Reset />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
