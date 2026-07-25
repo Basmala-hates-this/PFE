dfede15e31682ef5dfcee294f98d696ccbcdc3de
basmala <laouaribasmala75@gmail.com>
2026-07-25 14:26:18 +0100

    removed the fallback so maybe it fixes it?|| "*" ..... i highly doubt this could be the issue

--- something-backend/server.js
+++ something-backend/server.js
@@ -13,7 +13,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "*",
+    origin: process.env.FRONTEND_URL,
+    // || "*",
     methods: ["GET", "POST"],
     credentials: true, // needed so the cookie actually gets sent on the handshake
   },
